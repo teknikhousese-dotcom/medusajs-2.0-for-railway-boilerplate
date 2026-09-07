c3082c09501d2c039f108ae0d919de6a3311b7a3
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 14:48:50 +0200

    Update index.tsx

--- storefront/src/modules/checkout/components/payment/index.tsx
+++ storefront/src/modules/checkout/components/payment/index.tsx
@@ -126,7 +126,7 @@ const Payment = ({
             }
           )}
         >
-          Payment
+          Betalning
           {!isOpen && paymentReady && <CheckCircleSolid />}
         </Heading>
         {!isOpen && paymentReady && (
@@ -136,7 +136,7 @@ const Payment = ({
               className="text-ui-fg-interactive hover:text-ui-fg-interactive-hover"
               data-testid="edit-payment-button"
             >
-              Edit
+              Ändra
             </button>
           </Text>
         )}
@@ -167,7 +167,7 @@ const Payment = ({
               {isStripe && stripeReady && (
                 <div className="mt-5 transition-all duration-150 ease-in-out">
                   <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                    Enter your card details:
+                    Ange dina kortuppgifter:
                   </Text>
 
                   <CardElement
@@ -189,13 +189,13 @@ const Payment = ({
           {paidByGiftcard && (
             <div className="flex flex-col w-1/3">
               <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                Payment method
+                Betalsätt
               </Text>
               <Text
                 className="txt-medium text-ui-fg-subtle"
                 data-testid="payment-method-summary"
               >
-                Gift card
+                Presentkort
               </Text>
             </div>
           )}
@@ -218,7 +218,7 @@ const Payment = ({
           >
             {!activeSession && isStripeFunc(selectedPaymentMethod)
               ? " Enter card details"
-              : "Continue to review"}
+              : "Fortsätt till granskning"}
           </Button>
         </div>
 
@@ -227,7 +227,7 @@ const Payment = ({
             <div className="flex items-start gap-x-1 w-full">
               <div className="flex flex-col w-1/3">
                 <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                  Payment method
+                  Betalsätt
                 </Text>
                 <Text
                   className="txt-medium text-ui-fg-subtle"
@@ -239,7 +239,7 @@ const Payment = ({
               </div>
               <div className="flex flex-col w-1/3">
                 <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                  Payment details
+                  Betalningsuppgifter
                 </Text>
                 <div
                   className="flex gap-2 txt-medium text-ui-fg-subtle items-center"
@@ -261,13 +261,13 @@ const Payment = ({
           ) : paidByGiftcard ? (
             <div className="flex flex-col w-1/3">
               <Text className="txt-medium-plus text-ui-fg-base mb-1">
-                Payment method
+                Betalsätt
               </Text>
               <Text
                 className="txt-medium text-ui-fg-subtle"
                 data-testid="payment-method-summary"
               >
-                Gift card
+                Presentkort
               </Text>
             </div>
           ) : null}
