962403b858485beaa241509d9a624e8140e699bc
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:56:34 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/account/@dashboard/page.tsx
+++ storefront/src/app/[countryCode]/(main)/account/@dashboard/page.tsx
@@ -6,8 +6,8 @@ import { getCustomer } from "@lib/data/customer"
 import { listOrders } from "@lib/data/orders"
 
 export const metadata: Metadata = {
-  title: "Account",
-  description: "Overview of your account activity.",
+  title: "Mitt konto",
+  description: "Översikt över din kontoaktivitet.",
 }
 
 export default async function OverviewTemplate() {
