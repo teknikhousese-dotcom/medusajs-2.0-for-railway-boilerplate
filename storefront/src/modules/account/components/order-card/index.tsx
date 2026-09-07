b9bb907b8b41e0cebbdaaf7520ac86a2c61044f3
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:18:27 +0200

    Update index.tsx

--- storefront/src/modules/account/components/order-card/index.tsx
+++ storefront/src/modules/account/components/order-card/index.tsx
@@ -41,7 +41,7 @@ const OrderCard = ({ order }: OrderCardProps) => {
           })}
         </span>
         <span className="pl-2">{`${numberOfLines} ${
-          numberOfLines > 1 ? "items" : "item"
+          numberOfLines > 1 ? "varor" : "vara"
         }`}</span>
       </div>
       <div className="grid grid-cols-2 small:grid-cols-4 gap-4 my-4">
@@ -71,14 +71,14 @@ const OrderCard = ({ order }: OrderCardProps) => {
             <span className="text-small-regular text-ui-fg-base">
               + {numberOfLines - 4}
             </span>
-            <span className="text-small-regular text-ui-fg-base">more</span>
+            <span className="text-small-regular text-ui-fg-base">fler</span>
           </div>
         )}
       </div>
       <div className="flex justify-end">
         <LocalizedClientLink href={`/account/orders/details/${order.id}`}>
           <Button data-testid="order-details-link" variant="secondary">
-            See details
+            Visa detaljer
           </Button>
         </LocalizedClientLink>
       </div>
