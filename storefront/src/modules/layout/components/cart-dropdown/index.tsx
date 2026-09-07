8e9212db0fd9ed9124793ce62539798efec2ef18
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 14:33:04 +0200

    Update index.tsx

--- storefront/src/modules/layout/components/cart-dropdown/index.tsx
+++ storefront/src/modules/layout/components/cart-dropdown/index.tsx
@@ -85,7 +85,7 @@ const CartDropdown = ({
             className="hover:text-ui-fg-base"
             href="/cart"
             data-testid="nav-cart-link"
-          >{`Cart (${totalItems})`}</LocalizedClientLink>
+          >{`Varukorg (${totalItems})`}</LocalizedClientLink>
         </Popover.Button>
         <Transition
           show={cartDropdownOpen}
@@ -103,7 +103,7 @@ const CartDropdown = ({
             data-testid="nav-cart-dropdown"
           >
             <div className="p-4 flex items-center justify-center">
-              <h3 className="text-large-semi">Cart</h3>
+              <h3 className="text-large-semi">Varukorg</h3>
             </div>
             {cartState && cartState.items?.length ? (
               <>
@@ -151,7 +151,7 @@ const CartDropdown = ({
                                   data-testid="cart-item-quantity"
                                   data-value={item.quantity}
                                 >
-                                  Quantity: {item.quantity}
+                                  Antal: {item.quantity}
                                 </span>
                               </div>
                               <div className="flex justify-end">
@@ -164,7 +164,7 @@ const CartDropdown = ({
                             className="mt-1"
                             data-testid="cart-item-remove-button"
                           >
-                            Remove
+                            Ta bort
                           </DeleteButton>
                         </div>
                       </div>
@@ -173,8 +173,8 @@ const CartDropdown = ({
                 <div className="p-4 flex flex-col gap-y-4 text-small-regular">
                   <div className="flex items-center justify-between">
                     <span className="text-ui-fg-base font-semibold">
-                      Subtotal{" "}
-                      <span className="font-normal">(excl. taxes)</span>
+                      Delsumma{" "}
+                      <span className="font-normal">(exkl. moms)</span>
                     </span>
                     <span
                       className="text-large-semi"
@@ -193,7 +193,7 @@ const CartDropdown = ({
                       size="large"
                       data-testid="go-to-cart-button"
                     >
-                      Go to cart
+                      Till varukorgen
                     </Button>
                   </LocalizedClientLink>
                 </div>
@@ -204,12 +204,12 @@ const CartDropdown = ({
                   <div className="bg-gray-900 text-small-regular flex items-center justify-center w-6 h-6 rounded-full text-white">
                     <span>0</span>
                   </div>
-                  <span>Your shopping bag is empty.</span>
+                  <span>Din varukorg är tom.</span>
                   <div>
                     <LocalizedClientLink href="/store">
                       <>
-                        <span className="sr-only">Go to all products page</span>
-                        <Button onClick={close}>Explore products</Button>
+                        <span className="sr-only">Gå till alla produkter</span>
+                        <Button onClick={close}>Utforska produkter</Button>
                       </>
                     </LocalizedClientLink>
                   </div>
