cf0c1d8a556c3725f7ebe4c71411258c8e57ecb6
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:55:48 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/cart/page.tsx
+++ storefront/src/app/[countryCode]/(main)/cart/page.tsx
@@ -6,8 +6,8 @@ import { HttpTypes } from "@medusajs/types"
 import { getCustomer } from "@lib/data/customer"
 
 export const metadata: Metadata = {
-  title: "Cart",
-  description: "View your cart",
+  title: "Varukorg",
+  description: "Visa din varukorg",
 }
 
 const fetchCart = async () => {
