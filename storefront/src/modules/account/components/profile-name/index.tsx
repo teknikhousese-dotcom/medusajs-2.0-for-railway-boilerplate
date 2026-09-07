3fbea789769e494a90e24c220b8146828fe4b2b7
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:19:57 +0200

    Update index.tsx

--- storefront/src/modules/account/components/profile-name/index.tsx
+++ storefront/src/modules/account/components/profile-name/index.tsx
@@ -48,7 +48,7 @@ const ProfileName: React.FC<MyInformationProps> = ({ customer }) => {
   return (
     <form action={formAction} className="w-full overflow-visible">
       <AccountInfo
-        label="Name"
+        label="Namn"
         currentInfo={`${customer.first_name} ${customer.last_name}`}
         isSuccess={successState}
         isError={!!state?.error}
@@ -57,14 +57,14 @@ const ProfileName: React.FC<MyInformationProps> = ({ customer }) => {
       >
         <div className="grid grid-cols-2 gap-x-4">
           <Input
-            label="First name"
+            label="Förnamn"
             name="first_name"
             required
             defaultValue={customer.first_name ?? ""}
             data-testid="first-name-input"
           />
           <Input
-            label="Last name"
+            label="Efternamn"
             name="last_name"
             required
             defaultValue={customer.last_name ?? ""}
