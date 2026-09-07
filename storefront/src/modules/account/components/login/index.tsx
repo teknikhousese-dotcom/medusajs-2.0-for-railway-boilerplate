9401817fd582485367c24987598e09319b3598eb
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:12:57 +0200

    Update index.tsx

--- storefront/src/modules/account/components/login/index.tsx
+++ storefront/src/modules/account/components/login/index.tsx
@@ -20,14 +20,14 @@ const Login = ({ setCurrentView }: Props) => {
       className="max-w-sm w-full flex flex-col items-center"
       data-testid="login-page"
     >
-      <h1 className="text-large-semi uppercase mb-6">Welcome back</h1>
+      <h1 className="text-large-semi uppercase mb-6">Välkommen tillbaka</h1>
       <p className="text-center text-base-regular text-ui-fg-base mb-8">
-        Sign in to access an enhanced shopping experience.
+        Logga in för en bättre shoppingupplevelse.
       </p>
       <form className="w-full" action={formAction}>
         <div className="flex flex-col w-full gap-y-2">
           <Input
-            label="Email"
+            label="E-post"
             name="email"
             type="email"
             title="Enter a valid email address."
@@ -36,7 +36,7 @@ const Login = ({ setCurrentView }: Props) => {
             data-testid="email-input"
           />
           <Input
-            label="Password"
+            label="Lösenord"
             name="password"
             type="password"
             autoComplete="current-password"
@@ -46,7 +46,7 @@ const Login = ({ setCurrentView }: Props) => {
         </div>
         <ErrorMessage error={message} data-testid="login-error-message" />
         <SubmitButton data-testid="sign-in-button" className="w-full mt-6">
-          Sign in
+          Logga in
         </SubmitButton>
       </form>
       <button
@@ -57,13 +57,13 @@ const Login = ({ setCurrentView }: Props) => {
         Forgot your password?
       </button>
       <span className="text-center text-ui-fg-base text-small-regular mt-6">
-        Not a member?{" "}
+        Inte medlem än?{" "}
         <button
           onClick={() => setCurrentView(LOGIN_VIEW.REGISTER)}
           className="underline"
           data-testid="register-button"
         >
-          Join us
+          Bli medlem
         </button>
         .
       </span>
