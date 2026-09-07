4780a57bbe48ea8f2a20cd83de63c827eb4069a8
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 14:37:54 +0200

    Update index.tsx

--- storefront/src/modules/cart/components/empty-cart-message/index.tsx
+++ storefront/src/modules/cart/components/empty-cart-message/index.tsx
@@ -9,14 +9,13 @@ const EmptyCartMessage = () => {
         level="h1"
         className="flex flex-row text-3xl-regular gap-x-2 items-baseline"
       >
-        Cart
+        Varukorg
       </Heading>
       <Text className="text-base-regular mt-4 mb-6 max-w-[32rem]">
-        You don&apos;t have anything in your cart. Let&apos;s change that, use
-        the link below to start browsing our products.
+        Din varukorg är tom just nu. Använd länken nedan för att börja botanisera bland våra produkter.
       </Text>
       <div>
-        <InteractiveLink href="/store">Explore products</InteractiveLink>
+        <InteractiveLink href="/store">Utforska produkter</InteractiveLink>
       </div>
     </div>
   )
