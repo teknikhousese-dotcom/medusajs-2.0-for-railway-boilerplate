35320c1799b5120868011cbf602de54c32c8f8a2
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:24:48 +0200

    Update account-layout.tsx

--- storefront/src/modules/account/templates/account-layout.tsx
+++ storefront/src/modules/account/templates/account-layout.tsx
@@ -23,15 +23,15 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
         </div>
         <div className="flex flex-col small:flex-row items-end justify-between small:border-t border-gray-200 py-12 gap-8">
           <div>
-            <h3 className="text-xl-semi mb-4">Got questions?</h3>
+            <h3 className="text-xl-semi mb-4">Frågor?</h3>
             <span className="txt-medium">
-              You can find frequently asked questions and answers on our
-              customer service page.
+              Du hittar vanliga frågor och svar på vår
+              kundtjänstsida.
             </span>
           </div>
           <div>
-            <UnderlineLink href="/customer-service">
-              Customer Service
+            <UnderlineLink href="/info/kundtjanst">
+              Kundtjänst
             </UnderlineLink>
           </div>
         </div>
