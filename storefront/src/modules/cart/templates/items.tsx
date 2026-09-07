100229a6936b03bfadc9f7ec6722a33bcea5879a
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 14:39:05 +0200

    Update items.tsx

--- storefront/src/modules/cart/templates/items.tsx
+++ storefront/src/modules/cart/templates/items.tsx
@@ -18,14 +18,14 @@ const ItemsTemplate = ({ items }: ItemsTemplateProps) => {
       <Table>
         <Table.Header className="border-t-0">
           <Table.Row className="text-ui-fg-subtle txt-medium-plus">
-            <Table.HeaderCell className="!pl-0">Item</Table.HeaderCell>
+            <Table.HeaderCell className="!pl-0">Vara</Table.HeaderCell>
             <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Quantity</Table.HeaderCell>
+            <Table.HeaderCell>Antal</Table.HeaderCell>
             <Table.HeaderCell className="hidden small:table-cell">
-              Price
+              Pris
             </Table.HeaderCell>
             <Table.HeaderCell className="!pr-0 text-right">
-              Total
+              Summa
             </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
