e830562535c0d4a1a4ae8470c4240b075a9743fa
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-08 01:22:59 +0200

    Update index.tsx

--- storefront/src/modules/search/components/search-box-wrapper/index.tsx
+++ storefront/src/modules/search/components/search-box-wrapper/index.tsx
@@ -33,7 +33,7 @@ type SearchBoxProps = {
 
 const SearchBoxWrapper = ({
   children,
-  placeholder = "Search products...",
+  placeholder = "Sök produkter...",
   ...rest
 }: SearchBoxProps) => {
   const { query, refine } = useSearchBox(rest)
