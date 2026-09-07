0dfb88ac2b6fddbf77a21db93f5eb503143d5b29
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 18:10:18 +0200

    Update edit-address-modal.tsx

--- storefront/src/modules/account/components/address-card/edit-address-modal.tsx
+++ storefront/src/modules/account/components/address-card/edit-address-modal.tsx
@@ -116,21 +116,21 @@ const EditAddress: React.FC<EditAddressProps> = ({
             data-testid="address-delete-button"
           >
             {removing ? <Spinner /> : <Trash />}
-            Remove
+            Ta bort
           </button>
         </div>
       </div>
 
       <Modal isOpen={state} close={close} data-testid="edit-address-modal">
         <Modal.Title>
-          <Heading className="mb-2">Edit address</Heading>
+          <Heading className="mb-2">Redigera adress</Heading>
         </Modal.Title>
         <form action={formAction}>
           <Modal.Body>
             <div className="grid grid-cols-1 gap-y-2">
               <div className="grid grid-cols-2 gap-x-2">
                 <Input
-                  label="First name"
+                  label="Förnamn"
                   name="first_name"
                   required
                   autoComplete="given-name"
@@ -138,7 +138,7 @@ const EditAddress: React.FC<EditAddressProps> = ({
                   data-testid="first-name-input"
                 />
                 <Input
-                  label="Last name"
+                  label="Efternamn"
                   name="last_name"
                   required
                   autoComplete="family-name"
@@ -147,14 +147,14 @@ const EditAddress: React.FC<EditAddressProps> = ({
                 />
               </div>
               <Input
-                label="Company"
+                label="Företag"
                 name="company"
                 autoComplete="organization"
                 defaultValue={address.company || undefined}
                 data-testid="company-input"
               />
               <Input
-                label="Address"
+                label="Adress"
                 name="address_1"
                 required
                 autoComplete="address-line1"
@@ -162,7 +162,7 @@ const EditAddress: React.FC<EditAddressProps> = ({
                 data-testid="address-1-input"
               />
               <Input
-                label="Apartment, suite, etc."
+                label="Lägenhet, våning, etc."
                 name="address_2"
                 autoComplete="address-line2"
                 defaultValue={address.address_2 || undefined}
@@ -170,7 +170,7 @@ const EditAddress: React.FC<EditAddressProps> = ({
               />
               <div className="grid grid-cols-[144px_1fr] gap-x-2">
                 <Input
-                  label="Postal code"
+                  label="Postnummer"
                   name="postal_code"
                   required
                   autoComplete="postal-code"
@@ -178,7 +178,7 @@ const EditAddress: React.FC<EditAddressProps> = ({
                   data-testid="postal-code-input"
                 />
                 <Input
-                  label="City"
+                  label="Ort"
                   name="city"
                   required
                   autoComplete="locality"
@@ -187,7 +187,7 @@ const EditAddress: React.FC<EditAddressProps> = ({
                 />
               </div>
               <Input
-                label="Province / State"
+                label="Län"
                 name="province"
                 autoComplete="address-level1"
                 defaultValue={address.province || undefined}
@@ -202,7 +202,7 @@ const EditAddress: React.FC<EditAddressProps> = ({
                 data-testid="country-select"
               />
               <Input
-                label="Phone"
+                label="Telefon"
                 name="phone"
                 autoComplete="phone"
                 defaultValue={address.phone || undefined}
@@ -224,9 +224,9 @@ const EditAddress: React.FC<EditAddressProps> = ({
                 className="h-10"
                 data-testid="cancel-button"
               >
-                Cancel
+                Avbryt
               </Button>
-              <SubmitButton data-testid="save-button">Save</SubmitButton>
+              <SubmitButton data-testid="save-button">Spara</SubmitButton>
             </div>
           </Modal.Footer>
         </form>
