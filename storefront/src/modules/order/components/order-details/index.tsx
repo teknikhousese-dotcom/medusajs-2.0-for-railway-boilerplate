6d1c2f30e9584992c144e8c32a721e2160ccf97d
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:10:22 +0200

    Update index.tsx

--- storefront/src/modules/order/components/order-details/index.tsx
+++ storefront/src/modules/order/components/order-details/index.tsx
@@ -16,7 +16,7 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
   return (
     <div>
       <Text>
-        We have sent the order confirmation details to{" "}
+        Vi har skickat orderbekräftelsen till{" "}
         <span
           className="text-ui-fg-medium-plus font-semibold"
           data-testid="order-email"
@@ -26,27 +26,27 @@ const OrderDetails = ({ order, showStatus }: OrderDetailsProps) => {
         .
       </Text>
       <Text className="mt-2">
-        Order date:{" "}
+        Orderdatum:{" "}
         <span data-testid="order-date">
           {new Date(order.created_at).toDateString()}
         </span>
       </Text>
       <Text className="mt-2 text-ui-fg-interactive">
-        Order number: <span data-testid="order-id">{order.display_id}</span>
+        Ordernummer: <span data-testid="order-id">{order.display_id}</span>
       </Text>
 
       <div className="flex items-center text-compact-small gap-x-4 mt-4">
         {showStatus && (
           <>
             <Text>
-              Order status:{" "}
+              Orderstatus:{" "}
               <span className="text-ui-fg-subtle " data-testid="order-status">
                 {/* TODO: Check where the statuses should come from */}
                 {/* {formatStatus(order.fulfillment_status)} */}
               </span>
             </Text>
             <Text>
-              Payment status:{" "}
+              Betalningsstatus:{" "}
               <span
                 className="text-ui-fg-subtle "
                 sata-testid="order-payment-status"
