d2fa9919a32e0fd3406b3e6009733e93e09eea9a
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:06:27 +0200

    Update index.tsx

--- storefront/src/modules/products/components/related-products/index.tsx
+++ storefront/src/modules/products/components/related-products/index.tsx
@@ -50,10 +50,10 @@ export default async function RelatedProducts({
     <div className="product-page-constraint">
       <div className="flex flex-col items-center text-center mb-16">
         <span className="text-base-regular text-gray-600 mb-6">
-          Related products
+          Liknande produkter
         </span>
         <p className="text-2xl-regular text-ui-fg-base max-w-lg">
-          You might also want to check out these products.
+          Du kanske också vill kolla in dessa produkter.
         </p>
       </div>
 
