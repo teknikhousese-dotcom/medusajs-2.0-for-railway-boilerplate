4e88de4435647cd9bf806f41f9e1fc5e2f9eff7a
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-07 17:08:14 +0200

    Update index.tsx

--- storefront/src/modules/checkout/components/discount-code/index.tsx
+++ storefront/src/modules/checkout/components/discount-code/index.tsx
@@ -66,7 +66,7 @@ const DiscountCode: React.FC<DiscountCodeProps> = ({ cart }) => {
               className="txt-medium text-ui-fg-interactive hover:text-ui-fg-interactive-hover"
               data-testid="add-discount-button"
             >
-              Add Promotion Code(s)
+              Lägg till rabattkod
             </button>
 
             {/* <Tooltip content="You can add multiple promotion codes">
@@ -89,7 +89,7 @@ const DiscountCode: React.FC<DiscountCodeProps> = ({ cart }) => {
                   variant="secondary"
                   data-testid="discount-apply-button"
                 >
-                  Apply
+                  Använd
                 </SubmitButton>
               </div>
 
@@ -105,7 +105,7 @@ const DiscountCode: React.FC<DiscountCodeProps> = ({ cart }) => {
           <div className="w-full flex items-center">
             <div className="flex flex-col w-full">
               <Heading className="txt-medium mb-2">
-                Promotion(s) applied:
+                Rabattkod(er) tillämpad:
               </Heading>
 
               {promotions.map((promotion) => {
@@ -161,7 +161,7 @@ const DiscountCode: React.FC<DiscountCodeProps> = ({ cart }) => {
                       >
                         <Trash size={14} />
                         <span className="sr-only">
-                          Remove discount code from order
+                          Ta bort rabattkod från beställning
                         </span>
                       </button>
                     )}
