525d8b826acb6887bdeb405830e359fa05445d20
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-10 00:54:34 +0200

    Update index.tsx

--- storefront/src/modules/cart/components/sign-in-prompt/index.tsx
+++ storefront/src/modules/cart/components/sign-in-prompt/index.tsx
@@ -6,16 +6,16 @@ const SignInPrompt = () => {
     <div className="bg-white flex items-center justify-between">
       <div>
         <Heading level="h2" className="txt-xlarge">
-          Already have an account?
+          Har du redan ett konto?
         </Heading>
         <Text className="txt-medium text-ui-fg-subtle mt-2">
-          Sign in for a better experience.
+          Logga in för en bättre upplevelse.
         </Text>
       </div>
       <div>
         <LocalizedClientLink href="/account">
           <Button variant="secondary" className="h-10" data-testid="sign-in-button">
-            Sign in
+            Logga in
           </Button>
         </LocalizedClientLink>
       </div>
