b53e0b24c939588741a5d5340d1927e9183aa24d
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-11 12:54:22 +0200

    Update not-found.tsx

--- storefront/src/app/[countryCode]/(main)/cart/not-found.tsx
+++ storefront/src/app/[countryCode]/(main)/cart/not-found.tsx
@@ -10,12 +10,12 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
+      <h1 className="text-2xl-semi text-ui-fg-base">Sidan hittades inte</h1>
       <p className="text-small-regular text-ui-fg-base">
         The cart you tried to access does not exist. Clear your cookies and try
         again.
       </p>
-      <InteractiveLink href="/">Go to frontpage</InteractiveLink>
+      <InteractiveLink href="/">Till startsidan</InteractiveLink>
     </div>
   )
 }
