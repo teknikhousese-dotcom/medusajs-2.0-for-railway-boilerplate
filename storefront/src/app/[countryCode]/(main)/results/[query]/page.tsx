150508b4c992c2c5b2db7971d8c199be7b593135
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-11 14:21:33 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/results/[query]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/results/[query]/page.tsx
@@ -6,8 +6,8 @@ import { search } from "@modules/search/actions"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 
 export const metadata: Metadata = {
-  title: "Search",
-  description: "Explore all of our products.",
+  title: "Sök",
+  description: "Sök i hela vårt sortiment.",
 }
 
 type Params = {
