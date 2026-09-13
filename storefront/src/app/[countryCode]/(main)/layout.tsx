d98ad90f67f7ecdb549efb9f4e59bef0d95bdb79
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-13 21:39:03 +0200

    Update layout.tsx

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 
 import Footer from "@modules/layout/templates/footer"
 import Nav from "@modules/layout/templates/nav"
+import CookieConsent from "@modules/layout/components/cookie-consent"
 import { getBaseURL } from "@lib/util/env"
 
 export const metadata: Metadata = {
@@ -14,6 +15,7 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
       <Nav />
       {props.children}
       <Footer />
+      <CookieConsent />
     </>
   )
 }
