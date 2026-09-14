f562849d0719a20a88d2a488155f244ff26449e2
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-14 12:45:56 +0200

    Update constants.tsx

--- storefront/src/lib/constants.tsx
+++ storefront/src/lib/constants.tsx
@@ -11,7 +11,7 @@ export const paymentInfoMap: Record<
   { title: string; icon: React.JSX.Element }
 > = {
   pp_stripe_stripe: {
-    title: "Credit card",
+    title: "Kortbetalning",
     icon: <CreditCard />,
   },
   "pp_stripe-ideal_stripe": {
@@ -27,7 +27,7 @@ export const paymentInfoMap: Record<
     icon: <PayPal />,
   },
   pp_system_default: {
-    title: "Manual Payment",
+    title: "Faktura",
     icon: <CreditCard />,
   },
   // Add more payment providers here
