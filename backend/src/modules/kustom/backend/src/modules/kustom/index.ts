0735f5078ed522b538852414466934fc0141af51
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-24 20:49:07 +0200

    Delete backend/src/modules/kustom/backend/src/modules/kustom/index.ts

--- backend/src/modules/kustom/backend/src/modules/kustom/index.ts
+++
@@ -1,6 +0,0 @@
-import { ModuleProvider, Modules } from "@medusajs/framework/utils"
-import KustomProviderService from "./service"
-
-export default ModuleProvider(Modules.PAYMENT, {
-  services: [KustomProviderService],
-})
