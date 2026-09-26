10f71e4f253c33a3d4c4aeb97c49c0f31292f64b
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-26 11:10:06 +0200

    Cart page: responsive columns, summary card, sticky only on desktop

--- storefront/src/modules/cart/templates/index.tsx
+++ storefront/src/modules/cart/templates/index.tsx
@@ -13,11 +13,11 @@ const CartTemplate = ({
   customer: HttpTypes.StoreCustomer | null
 }) => {
   return (
-    <div className="py-12">
+    <div className="py-6 small:py-12">
       <div className="content-container" data-testid="cart-container">
         {cart?.items?.length ? (
-          <div className="grid grid-cols-1 small:grid-cols-[1fr_360px] gap-x-40">
-            <div className="flex flex-col bg-white py-6 gap-y-6">
+          <div className="grid grid-cols-1 small:grid-cols-[minmax(0,1fr)_340px] medium:grid-cols-[minmax(0,1fr)_380px] gap-x-10 medium:gap-x-20">
+            <div className="flex flex-col bg-white py-2 small:py-6 gap-y-6">
               {!customer && (
                 <>
                   <SignInPrompt />
@@ -27,10 +27,10 @@ const CartTemplate = ({
               <ItemsTemplate items={cart?.items} />
             </div>
             <div className="relative">
-              <div className="flex flex-col gap-y-8 sticky top-12">
+              <div className="flex flex-col gap-y-8 small:sticky small:top-28">
                 {cart && cart.region && (
                   <>
-                    <div className="bg-white py-6">
+                    <div className="bg-white py-6 small:rounded-2xl small:border small:border-gray-200 small:p-6">
                       <Summary cart={cart as any} />
                     </div>
                   </>
