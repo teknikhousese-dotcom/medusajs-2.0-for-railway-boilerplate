e7890d7b5b7f9fa9260601e17b18b4ed41da393e
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-26 11:18:43 +0200

    Konto: centrerad inloggning, länk till Spåra order och kontakt

--- storefront/src/modules/account/templates/account-layout.tsx
+++ storefront/src/modules/account/templates/account-layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import UnderlineLink from "@modules/common/components/interactive-link"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 
 import AccountNav from "../components/account-nav"
 import { HttpTypes } from "@medusajs/types"
@@ -15,22 +16,21 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
   children,
 }) => {
   return (
-    <div className="flex-1 small:py-12" data-testid="account-page">
+    <div className="flex-1 lg:py-12" data-testid="account-page">
       <div className="flex-1 content-container h-full max-w-5xl mx-auto bg-white flex flex-col">
-        <div className="grid grid-cols-1  small:grid-cols-[240px_1fr] py-12">
-          <div>{customer && <AccountNav customer={customer} />}</div>
-          <div className="flex-1">{children}</div>
+        <div className={customer ? "grid grid-cols-1 small:grid-cols-[240px_1fr] py-8 sm:py-12" : "py-8 sm:py-12"}>
+          {customer && <div><AccountNav customer={customer} /></div>}
+          <div className="flex-1 min-w-0">{children}</div>
         </div>
-        <div className="flex flex-col small:flex-row items-end justify-between small:border-t border-gray-200 py-12 gap-8">
+        <div className="flex flex-col small:flex-row small:items-end justify-between border-t border-gray-200 py-8 sm:py-12 gap-6">
           <div>
             <h3 className="text-xl-semi mb-4">Frågor?</h3>
-            <span className="txt-medium">
-              Du hittar vanliga frågor och svar på vår
-              kundtjänstsida.
+            <span className="txt-medium text-ui-fg-subtle">
+              Undrar du var paketet är? <LocalizedClientLink href="/orderstatus" className="text-[#D10000] underline">Spåra din order</LocalizedClientLink>. Annars hittar du svar och kontaktuppgifter hos kundtjänst.
             </span>
           </div>
           <div>
-            <UnderlineLink href="/info/kundtjanst">
+            <UnderlineLink href="/contact">
               Kundtjänst
             </UnderlineLink>
           </div>
