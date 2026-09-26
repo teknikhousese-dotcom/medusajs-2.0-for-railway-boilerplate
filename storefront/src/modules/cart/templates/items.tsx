f08948805ca0a9e8e7e5fe9b90f31784d1203189
teknikhousese-dotcom <teknikhouse.se@gmail.com>
2026-09-26 11:18:02 +0200

    Cart items list: div layout instead of table so rows fit mobile

--- storefront/src/modules/cart/templates/items.tsx
+++ storefront/src/modules/cart/templates/items.tsx
@@ -1,9 +1,8 @@
 import repeat from "@lib/util/repeat"
 import { HttpTypes } from "@medusajs/types"
-import { Heading, Table } from "@medusajs/ui"
+import { Heading } from "@medusajs/ui"
 
 import Item from "@modules/cart/components/item"
-import SkeletonLineItem from "@modules/skeletons/components/skeleton-line-item"
 
 type ItemsTemplateProps = {
   items?: HttpTypes.StoreCartLineItem[]
@@ -12,37 +11,29 @@ type ItemsTemplateProps = {
 const ItemsTemplate = ({ items }: ItemsTemplateProps) => {
   return (
     <div>
-      <div className="pb-3 flex items-center">
-        <Heading className="text-[2rem] leading-[2.75rem]">Varukorg</Heading>
+      <div className="pb-2 flex items-center">
+        <Heading className="text-[24px] small:text-[32px] font-semibold leading-tight text-[#14161C]">
+          Varukorg
+        </Heading>
       </div>
-      <Table>
-        <Table.Header className="border-t-0">
-          <Table.Row className="text-ui-fg-subtle txt-medium-plus">
-            <Table.HeaderCell className="!pl-0">Vara</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Antal</Table.HeaderCell>
-            <Table.HeaderCell className="hidden small:table-cell">
-              Pris
-            </Table.HeaderCell>
-            <Table.HeaderCell className="!pr-0 text-right">
-              Summa
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {items
-            ? items
-                .sort((a, b) => {
-                  return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
-                })
-                .map((item) => {
-                  return <Item key={item.id} item={item} />
-                })
-            : repeat(5).map((i) => {
-                return <SkeletonLineItem key={i} />
-              })}
-        </Table.Body>
-      </Table>
+      <div className="border-t border-gray-200">
+        {items
+          ? [...items]
+              .sort((a, b) => {
+                return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
+              })
+              .map((item) => {
+                return <Item key={item.id} item={item} />
+              })
+          : repeat(3).map((i) => {
+              return (
+                <div
+                  key={i}
+                  className="h-24 my-4 rounded-xl bg-gray-100 animate-pulse"
+                />
+              )
+            })}
+      </div>
     </div>
   )
 }
